d81648a2cf8e739a15ccb3e77e36fd0b7473ce7d
Su-Yang Yu <20665693+suyangyu@users.noreply.github.com>
2026-07-13 09:09:11 +0100

    POC added test-only protected proxy and get authenthorization routes

---
+++ app/controllers/testOnly/BackendBaseController.scala
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.testOnly
+
+import play.api.mvc.{ControllerComponents, RequestHeader}
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.mdc.RequestMdc
+import uk.gov.hmrc.play.bootstrap.controller.{Utf8MimeTypes, WithJsonBody}
+import uk.gov.hmrc.play.http.HeaderCarrierConverter
+
+import scala.util.Try
+
+/** A copy of uk.gov.hmrc.play.bootstrap.backend.controller.BackendBaseController
+  */
+trait BackendBaseController
+    extends play.api.mvc.BaseController
+    with Utf8MimeTypes
+    with WithJsonBody
+    with BackendHeaderCarrierProvider
+
+abstract class BackendController(
+    override val controllerComponents: ControllerComponents
+) extends BackendBaseController
+
+trait BackendHeaderCarrierProvider {
+  implicit protected def hc(implicit request: RequestHeader): HeaderCarrier = {
+    // This is also called by `JsonErrorHandler.onClientError` - see there why we need `Try`
+    Try(request.id).map(RequestMdc.initMdc)
+    HeaderCarrierConverter.fromRequest(request)
+  }
+}
